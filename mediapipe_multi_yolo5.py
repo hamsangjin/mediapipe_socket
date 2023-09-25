6ce7580d8edb2cb11282bdeb5608d657f7087bd3
hamsangjin <rhkstlash@naver.com>
2023-09-25 14:40:04 +0900

    주석 제거

--- mediapipe_multi_yolo5.py
+++ mediapipe_multi_yolo5.py
@@ -1,209 +1,117 @@
-# import os
-# import matplotlib.pyplot as plt
-# import numpy as np
-# from PIL import Image
-import cv2 
+import cv2
 import mediapipe as mp
+from mediapipe.python.solutions import holistic as mp_holistic
 import torch
-from socket import *
 import re
 import json
+from socket import *
 
-# -----------------------------------------------------
-# 랜드마크 추출 함수
-# 정규 표현식(x, y, z) 추출
-# landmarks에서 x, y, z만 추출
-# ([+-]?\d+\.\d+) 부분이 각각의 x, y, z 값을 추출하는데 사용
-# - 또는 + 부호의 유무와 상관없이 소수점이 포함된 숫자를 추출
 pattern = r"landmark {\s+x: ([+-]?\d+\.\d+)\s+y: ([+-]?\d+\.\d+)\s+z: ([+-]?\d+\.\d+)"
 
 def transformer(data, part):
-
-  # 랜드마크 전처리
   matches_list = []
 
-  # 전처리 전의 데이터를 matches_list에 하나씩 저장
   for i in range(len(data)):
     matches_list.append(re.findall(pattern, data[i]))
 
-  # face 제외하고 일단 사전형 생성
   output_data = {part[0]: [], part[1]: [], part[2]: []}
 
-  # left - right - pose 순
   for i, matches in enumerate(matches_list):
     for _, match in enumerate(matches):
         x, y, z = map(float, match)
         output_data[part[i]].append({"x": round(x, 3), "y": round(y, 3), "z": round(z, 3)})
 
-  # output_json = json.dumps(output_data, indent=2, ensure_ascii=False)
-
   return output_data
 
 # -----------------------------------------------------
 
-# 소켓 사전 연결
-port = 25001
+# port = 25001
+# serverSock = socket(AF_INET, SOCK_STREAM)
+# serverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+# serverSock.bind(('', port))
+# serverSock.listen(1)
+# connectionSock, addr = serverSock.accept()
 
-# AF는 주소 체계로, 거의 AF_INET 사용 AF_INET은 IPv4, AF_INET6은 IPv6를 의미
-serverSock = socket(AF_INET, SOCK_STREAM)
-
-# 재사용 대기시간 없애기
-serverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-
-# ''는 AF_INET에서 모든 인터페이스와 연결한다는 의미
-serverSock.bind(('', port))
-
-# 인자는 동시접속 허용 개수
-serverSock.listen(1)
-
-# client가 접속 요청할 때 결과값이 return됨 / accept() 실행시 새로운 소켓이 생성됨
-# 기존에 생성한 serverSock이 아닌 새로운 connectionSock를 통해서 데이터 주고받음
-connectionSock, addr = serverSock.accept()
-
-flag = False
+# flag = False
 
 # --------------------------------------
 
-# Torch Hub에서 'ultralytics/yolov5' 저장소로부터 YOLOv5 모델(s)을 불러오기
-# yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5x6')
-yolo_model.to(torch.device('cuda'))
+yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+# yolo_model.to(torch.device('cuda'))
 
-# YOLOv5 모델에 클래스 설정
-# 여기서는 인덱스 0인 하나의 클래스만 사용
-# 사람만 탐지한다는 뜻으로 받아들이면 됨
-yolo_model.classes = [0]
+yolo_model.classes=[0]
 
 mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
 cap = cv2.VideoCapture(0)
 
 holistic = []
+for i in range(5):
+  holistic.append(mp_holistic.Holistic(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.9,
+    model_complexity=1,
+    smooth_segmentation=False,
+    enable_segmentation=False,
+    refine_face_landmarks=False))
 
-holistic.append(mp_holistic.Holistic(
-  min_detection_confidence=0.5,
-  min_tracking_confidence=0.9,
-  model_complexity=1,
-  smooth_segmentation=False,
-  enable_segmentation=False,
-  refine_face_landmarks=False))
-
-holistic.append(mp_holistic.Holistic(
-  min_detection_confidence=0.5,
-  min_tracking_confidence=0.9,
-  model_complexity=1,
-  smooth_segmentation=False,
-  enable_segmentation=False,
-  refine_face_landmarks=False))
-
-holistic.append(mp_holistic.Holistic(
-  min_detection_confidence=0.5,
-  min_tracking_confidence=0.9,
-  model_complexity=1,
-  smooth_segmentation=False,
-  enable_segmentation=False,
-  refine_face_landmarks=False))
-
-holistic.append(mp_holistic.Holistic(
-  min_detection_confidence=0.5,
-  min_tracking_confidence=0.9,
-  model_complexity=1,
-  smooth_segmentation=False,
-  enable_segmentation=False,
-  refine_face_landmarks=False))
-
-# 웹캠으로부터 프레임을 처리하는 루프를 시작
-while cap.isOpened():
-    # 프레임을 읽기
+while cap.isOpened():    
     success, image = cap.read()
-    
-    # 프레임을 못 읽은 경우 기다리기 (캠 로딩 문제)
     if not success:
-        continue
+      continue
     
-    # 이미지를 저장할 빈 리스트 초기화
-    img_list = []
-    
-    # 여백 값 설정
-    MARGIN = 1
-    
-    image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False    
+
+    result = yolo_model(image)    
     
-    # YOLOv5 모델 사용해 객체 탐지 수행
-    result = yolo_model(image)
-    
-    image.flags.writeable = True
+    image.flags.writeable = True   
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    
-    # 감지된 객체(사람)들에 대해서 반복
-    part_data = []    # 한 프레임의 랜드마크 모음
+
+    MARGIN=10
+    part_data = []
 
     for i in range(len(result.xyxy[0].tolist())):
         xmin, ymin, xmax, ymax, confidence, clas = result.xyxy[0].tolist()[i]
 
-        # 자세 추정을 위한 mediapipe holistic 객체를 초기화
-        # 감지된 객체 이미지 부분 처리
-        results = holistic[i].process(image[int(ymin)+MARGIN:int(ymax)+MARGIN, int(xmin)+MARGIN:int(xmax)+MARGIN:])
-        
-        # 랜드마크를 그림
-        mp_drawing.draw_landmarks(
-            image[int(ymin)+MARGIN:int(ymax)+MARGIN, int(xmin)+MARGIN:int(xmax)+MARGIN:],
-            results.pose_landmarks,
-            mp_holistic.POSE_CONNECTIONS,
-            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
-            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
-        
-        # img_list에 이미지 부분 추가(왜 필요하지 ?)
-        # img_list.append(image[int(ymin):int(ymax), int(xmin):int(xmax):])
+        results = holistic[i].process(image[int(ymin)+MARGIN:int(ymax)+MARGIN,int(xmin)+MARGIN:int(xmax)+MARGIN:])
 
-        # --------------------------------------------
-        # 왼손, 오른손, 몸, 얼굴 좌표 추출해 str로 변환 후 저장
+        mp_drawing.draw_landmarks(image[int(ymin)+MARGIN:int(ymax)+MARGIN,int(xmin)+MARGIN:int(xmax)+MARGIN:], results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                              )
+        
         data_left = str(results.left_hand_landmarks)
         data_right = str(results.right_hand_landmarks)
         data_pose = str(results.pose_world_landmarks)
         data_face = str(results.face_landmarks)
 
-        # face 제외하고 전송
         temp = [data_left, data_right, data_pose]
 
-        # 해당 프레임 랜드마크 추가
         part_data.append(temp)
+        
+    # --------------------------------------
 
-    # s키로 landmark 전송 제어
-    if cv2.waitKey(5) & 0xFF == ord('s'):
-        flag = not flag
-
-    # flag가 True면 Landmarks 전송
-    # if flag:
-    # 소켓 통신을 이용한 좌표 전송
     landmarks = []
 
-    # []로 한 프레임에 잡힌 객체들 묶기
     for i in range(len(part_data)):
         landmarks.append(transformer(part_data[i], ["left", "right", "pose"]))
 
-    # {}로 또 묶기
     landmarks = {"data": landmarks}
-
-    # json으로 변환
     landmarks = json.dumps(landmarks, indent=2, ensure_ascii=False)
-    
-    # 전체 랜드마크 전송
-    connectionSock.send(landmarks.encode('utf-8'))
+    # print(landmarks)
+    # connectionSock.send(landmarks.encode('utf-8'))
 
-    # 랜드마크 화면 출력
+    # --------------------------------------
+
     cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
 
-    # 'Esc' 키를 누르면 종료
     if cv2.waitKey(5) & 0xFF == 27:
         break
 
-# 웹캠을 해제
 for h in holistic:
-   h.close()
+   h.close
 
-# 웹캠을 해제
 cap.release()
+cv2.destroyAllWindows()
--- mediapipe_single.py
+++ mediapipe_single.py
@@ -6,27 +6,14 @@ from socket import *
 import re
 import json
 
-# -----------------------------------------------------
-# 랜드마크 추출 함수
-# 정규 표현식(x, y, z) 추출
-# landmarks에서 x, y, z만 추출
-# ([+-]?\d+\.\d+) 부분이 각각의 x, y, z 값을 추출하는데 사용
-# - 또는 + 부호의 유무와 상관없이 소수점이 포함된 숫자를 추출
 pattern = r"landmark {\s+x: ([+-]?\d+\.\d+)\s+y: ([+-]?\d+\.\d+)\s+z: ([+-]?\d+\.\d+)"
 
 def transformer(data, part):
 
-  # 랜드마크 전처리
   matches_list = []
-
-  # 전처리 전의 데이터를 matches_list에 하나씩 저장
   for i in range(len(data)):
     matches_list.append(re.findall(pattern, data[i]))
-
-  # face 제외하고 일단 사전형 생성
   output_data = {part[0]: [], part[1]: [], part[2]: []}
-
-  # left - right - pose 순
   for i, matches in enumerate(matches_list):
     for _, match in enumerate(matches):
         x, y, z = map(float, match)
@@ -38,27 +25,12 @@ def transformer(data, part):
 
 # -----------------------------------------------------
 
-# 소켓 사전 연결
 port = 25001
-
-# AF는 주소 체계로, 거의 AF_INET 사용 AF_INET은 IPv4, AF_INET6은 IPv6를 의미
 serverSock = socket(AF_INET, SOCK_STREAM)
-
-# 재사용 대기시간 없애기
 serverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-
-# ''는 AF_INET에서 모든 인터페이스와 연결한다는 의미
 serverSock.bind(('', port))
-
-# 인자는 동시접속 허용 개수
 serverSock.listen(1)
-
-# client가 접속 요청할 때 결과값이 return됨 / accept() 실행시 새로운 소켓이 생성됨
-# 기존에 생성한 serverSock이 아닌 새로운 connectionSock를 통해서 데이터 주고받음
 connectionSock, addr = serverSock.accept()
-
-# 전송 유무 결정
-flag = False
 
 # --------------------------------------
 
@@ -66,43 +38,30 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
-# 웹캠으로 입력
 cap = cv2.VideoCapture(0)
 
-# 모델 초기화
 with mp_holistic.Holistic(
     min_detection_confidence=0.1,
     min_tracking_confidence=0.9,
-
-    # 모델 복잡성으로 0 ~ 2, 정확도와 소요시간은 복잡성이 늘어날수록 증가
     model_complexity=0,
-
     smooth_segmentation=True,
-
-    # 얼굴/손 외에도 분할 마스크를 생성하는 유무
     enable_segmentation=True,
-
-    # 눈과 입술 주변의 랜드마크 좌표를 더 세분화할 것인지
-    # github엔 fine_face_landmarks라고 되어있음
     refine_face_landmarks=True) as holistic:
 
   while cap.isOpened():
     success, image = cap.read()
     if not success:
       print("카메라 인식 불가")
-      # 카메라가 로딩중일 수도 있으므로 break가 아닌 continue로
       continue
 
-    # 성능을 향상시키기 위해, 선택적으로 참조로 전달하기 위해 이미지를 쓸 수 없는 것으로 표시하세요.
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     results = holistic.process(image)
 
-    # 영상 출력
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    # 눈코입 엣지
     mp_drawing.draw_landmarks(
         image,
         results.face_landmarks,
@@ -111,7 +70,6 @@ with mp_holistic.Holistic(
         connection_drawing_spec=mp_drawing_styles
         .get_default_face_mesh_contours_style())
     
-    # 얼굴 엣지
     mp_drawing.draw_landmarks(
         image,
         results.face_landmarks,
@@ -119,8 +77,7 @@ with mp_holistic.Holistic(
         landmark_drawing_spec=None,
         connection_drawing_spec=mp_drawing_styles
         .get_default_face_mesh_tesselation_style())
-    
-    # 오른손 포인트 추가
+
     mp_drawing.draw_landmarks(
         image,
         results.right_hand_landmarks,
@@ -128,7 +85,6 @@ with mp_holistic.Holistic(
         landmark_drawing_spec=mp_drawing_styles
         .get_default_hand_landmarks_style())    
     
-    # 왼손 포인트 추가
     mp_drawing.draw_landmarks(
       image,
       results.left_hand_landmarks,
@@ -136,7 +92,6 @@ with mp_holistic.Holistic(
       landmark_drawing_spec=mp_drawing_styles
       .get_default_hand_landmarks_style())  
 
-    # 포즈(손 닭발 포함)
     mp_drawing.draw_landmarks(
         image,
         results.pose_landmarks,
@@ -144,49 +99,22 @@ with mp_holistic.Holistic(
         landmark_drawing_spec=mp_drawing_styles
         .get_default_pose_landmarks_style())
 
-    # 좌우반전해 영상출력
     cv2.imshow('MediaPipe Holistic', cv2.flip(image, 1))
 
-    
-    
     # ------------------------------------
-    # 랜드마크 추출 및 전송
 
-    # s키로 landmark 전송 제어
-    if cv2.waitKey(5) & 0xFF == ord('s'):
-      flag = not flag
-    
-    # 왼손, 오른손, 몸, 얼굴 좌표 추출해 str로 변환 후 저장
     data_left = str(results.left_hand_landmarks)
     data_right = str(results.right_hand_landmarks)
     data_pose = str(results.pose_world_landmarks)
     data_face = str(results.face_landmarks)
     
-    # face 포함해서 전송
-    # part = ["face", "left", "right", "pose"]
     # part_data = [data_face, data_left, data_right, data_pose]
-
-    # face 제외하고 전송
     part_data = [data_left, data_right, data_pose]
+    landmarks = transformer(part_data, ["left", "right", "pose"])
+    # connectionSock.send(landmarks.encode('utf-8'))
 
-    # flag가 True면 Landmarks 전송
-    if flag:
-      # 소켓 통신을 이용한 좌표 전송
-      landmarks = transformer(part_data, ["left", "right", "pose"])
-
-      # 전체 랜드마크 전송
-      connectionSock.send(landmarks.encode('utf-8'))
-    
-    # txt파일로 출력
-    # f = open("pose_landmarks.txt", 'w')
-    # f.write(pose)
-    # f.close()
-
-    # --------------------------------------
-    # esc키를 누르면 종료되며 소켓 닫음
     if cv2.waitKey(5) & 0xFF == 27:
       serverSock.close()
-      connectionSock.close()
       break
 
 cap.release()
